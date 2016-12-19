9412411a528bdec68f2200856e861e642f739620
Rob Hesketh <rhesketh.uk@gmail.com>
2016-12-19 23:26:57 +0000

    Refactor exchange rates

--- lib/foreign_exchange/exchange_rate.rb
+++ lib/foreign_exchange/exchange_rate.rb
@@ -4,39 +4,40 @@ module ForeignExchange
   class ExchangeRate
     class << self
       attr_accessor :rates
-    end
 
-    def self.parse_rates(rates_path = nil)
-      rates_file_path = rates_path || File.join("storage", "rates.xml")
-      raise NoRatesFound unless File.exists?(rates_file_path)
 
-      self.rates = {}
+      def at(date, base_currency, counter_currency)
+        date_key = date.strftime("%Y-%m-%d")
+        raise UnknownDate unless rates.key?(date_key)
+        raise NoCurrencyData unless rates[date_key].key?(base_currency) && rates[date_key].key?(counter_currency)
 
-      rates_document = File.open(rates_file_path) { |f| Nokogiri::XML(f) }
-      daily_rates = rates_document.xpath("//xmlns:Cube[@time]")
-      daily_rates.each do |daily_rate|
-        date = daily_rate.attribute("time").value
+        base_rate = rates[date_key][base_currency]
+        counter_rate = rates[date_key][counter_currency]
 
-        rates[date] = {}
-        currencies = daily_rate.xpath(".//xmlns:Cube[@rate]")
-        currencies.each do |currency|
-          code = currency.attribute("currency").value
-          rate = currency.attribute("rate").value
+        return (1 / base_rate) * (counter_rate / 1)
+      end
 
-          rates[date][code] = rate.to_f
+      def parse_rates(rates_path = nil)
+        rates_file_path = rates_path || File.join("storage", "rates.xml")
+        raise NoRatesFound unless File.exists?(rates_file_path)
+
+        self.rates = {}
+
+        rates_document = File.open(rates_file_path) { |f| Nokogiri::XML(f) }
+        daily_rates = rates_document.xpath("//xmlns:Cube[@time]")
+        daily_rates.each do |daily_rate|
+          date = daily_rate.attribute("time").value
+
+          rates[date] = {}
+          currencies = daily_rate.xpath(".//xmlns:Cube[@rate]")
+          currencies.each do |currency|
+            code = currency.attribute("currency").value
+            rate = currency.attribute("rate").value
+
+            rates[date][code] = rate.to_f
+          end
         end
       end
-    end
-
-    def self.at(date, base_currency, counter_currency)
-      date_key = date.strftime("%Y-%m-%d")
-      raise UnknownDate unless rates.key?(date_key)
-      raise NoCurrencyData unless rates[date_key].key?(base_currency) && rates[date_key].key?(counter_currency)
-
-      base_rate = rates[date_key][base_currency]
-      counter_rate = rates[date_key][counter_currency]
-
-      return (1 / base_rate) * (counter_rate / 1)
     end
 
     class UnknownDate < StandardError; end
