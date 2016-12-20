615c924b60c26e31f80168bd434ab21f42c2c875
Rob Hesketh <rhesketh.uk@gmail.com>
2016-12-20 22:36:34 +0000

    Check to see if the storage directory exists, create it if not

--- lib/foreign_exchange/rate_downloader.rb
+++ lib/foreign_exchange/rate_downloader.rb
@@ -1,4 +1,5 @@
 require "open-uri"
+require 'fileutils'
 
 module ForeignExchange
   class RateDownloader
@@ -6,6 +7,11 @@ module ForeignExchange
       raise ConfigError.new("Rates url must be defined before rates can be downloaded") unless ForeignExchange.configuration.rates_url.is_a?(String)
 
       uri = URI(ForeignExchange.configuration.rates_url)
+
+      unless File.directory?("storage")
+        FileUtils.mkdir_p("storage")
+      end
+
       local_file = File.join("storage", "rates.xml")
 
       File.open(local_file, 'wb') do |file|
